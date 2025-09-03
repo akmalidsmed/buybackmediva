d94673cb2183a75bdf80648016a2c6882f2d5bec
akmalidsmed <akmalidsmed@gmail.com>
2025-09-03 11:56:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -573,13 +573,13 @@ total_item = len(df)  # jumlah baris item
 
 col1, col2, col3, col4 = st.columns(4)
 with col1:
-    st.markdown(f'<div class="metric-card metric-red"><h3>{total_qty}</h3><p>Total Qty</p></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="metric-card metric-red"><h3>{total_item}</h3><p>Total Item</p></div>', unsafe_allow_html=True)
 with col2:
-    st.markdown(f'<div class="metric-card metric-green"><h3>{total_qty_buyback}</h3><p>Sudah Buyback</p></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="metric-card metric-blue"><h3>{total_qty}</h3><p>Total Qty</p></div>', unsafe_allow_html=True)
 with col3:
-    st.markdown(f'<div class="metric-card metric-blue"><h3>{total_sisa_qty}</h3><p>Sisa Qty</p></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="metric-card metric-green"><h3>{total_qty_buyback}</h3><p>Sudah Buyback</p></div>', unsafe_allow_html=True)
 with col4:
-    st.markdown(f'<div class="metric-card metric-purple"><h3>{total_item}</h3><p>Total Item</p></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="metric-card metric-purple"><h3>{total_sisa_qty}</h3><p>Sisa Qty</p></div>', unsafe_allow_html=True)
 
 st.divider()
 
